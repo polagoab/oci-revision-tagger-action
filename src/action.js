4ddb12e94c05e536020d18cbfd838abc86e8b2c4
Per Landberg <per.landberg@polago.se>
2026-01-28 16:17:12 +0100

    Remove unused dependency @actions/github

--- src/action.js
+++ src/action.js
@@ -17,7 +17,6 @@
 'use strict';
 
 const core = require('@actions/core');
-const github = require('@actions/github');
 const util = require('node:util');
 const exec = util.promisify(require('child_process').exec);
 
